e279782a210444e0fd71afc2b0e4b8a75649dd1e
Pablo Blanco <p400580@gmail.com>
2026-04-24 18:06:32 -0300

    Commit inicial, codigo-enigma

---
+++ codigo-enigma/cesar.h
@@ -0,0 +1,10 @@
+#ifndef CESAR_H_INCLUDED
+#define CESAR_H_INCLUDED
+
+void descifrar(const char *texto, int desplazamiento, char *resultado);
+int encontrarDesplazamiento(const char *textoCifrado, const char *crib);
+
+#endif // CESAR_H_INCLUDED
+
+
+
